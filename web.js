ce8f62e5fc87d47b1aff6e599257348381f96642
John Cotter <john.cotter.3@gmail.com>
2014-04-03 04:11:38 +0000

    updated for newer express version

--- web.js
+++ web.js
@@ -5,6 +5,12 @@ var analyst = require('./routes/analyst')
   , firm = require('./routes/firm');
 var http = require('http');
 var path = require('path');
+var favicons = require('static-favicon');
+var bodyParser = require('body-parser');
+//var methodOverride = require('get-methodoverride');
+var router = require('app-router');
+var connect = require('connect');
+var serveStatic = require('serve-static');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/AnalystData');
@@ -32,24 +38,32 @@ var Firm = mongoose.model('Firm', firmSchema, 'TestData');
 var Analyst = mongoose.model('Analyst', firmSchema, 'TestData');
 
 
-var app = express.createServer(express.logger());
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-var port = process.env.PORT || 8080;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+var app = express();
+// Logging middleware
+app.use(function(request, response, next) {
+  console.log("In comes a " + request.method + " to " + request.url);
+  next();
 });
+
+app.set("views", __dirname + "/views");
+app.set('view engine', 'jade');
+app.use(favicons());
+//app.use(express.logger('dev'));
+app.use(bodyParser());
+//app.use(methodOverride());
+app.use(require('method-override')());
+//app.use(router(app));
+app.use(serveStatic(path.join(__dirname, 'public')));
+
+//var port = process.env.PORT || 8080;
+//app.listen(port, function() {
+//  console.log("Listening on " + port);
+//});
 
 
 app.get('/', routes.index);
 app.get('/firms', firm.avg(Firm, firmSchema));
 app.get('/firms/:firmName', firm.specific(Firm, firmSchema));
 app.get('/analysts', analyst.avg(Analyst, firmSchema));
+
+http.createServer(app).listen(8080);
