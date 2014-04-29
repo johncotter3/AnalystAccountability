acfd5d15a2602e275ec4eb9fe3be1c041663d7a0
John Cotter <john.cotter.3@gmail.com>
2014-04-29 15:45:09 +0000

    fixed some issues with how postings were styled

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,9 @@ exports.index = function(postSchema) {
 	    if (err) return console.error(err);
 	    // TODO: Handle error when no results are returned
 	    console.log(docs);
+	    for (var i=0; i< docs.length; i++){
+		docs[i].Post = docs[i].Post.substring(0,499);
+	    }
 	    res.render('index', {
 		"docs" : docs
 	    });
