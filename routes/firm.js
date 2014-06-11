a0f57f6acf0c7bdb82d1e0b9c051e42d6b5477cb
John Cotter <john.cotter.3@gmail.com>
2014-06-11 01:28:45 +0000

    Changed specific firm page to show individual ratings rather than their analysts (only until I can get analysts in my data set)

--- routes/firm.js
+++ routes/firm.js
@@ -29,57 +29,25 @@ exports.specific = function(Firm, firmSchema) {
     return function(req, res) {
 	if (req.session.user!=null && req.session.user.member == 'premium') {
             var firmName = req.params.firmName;
-            var o = {};
-            o.map = function() {
-		var key = this.Analyst
-		var value = {
-                    count: 1,
-                    qty: this.Percent_Diff
-		};
-		emit(key, value);
-            };
-            o.reduce = function(keySKU, countObjVals) {
-		reducedVal = { count: 0, qty: 0 };
-		for (var idx = 0; idx < countObjVals.length; idx++) {
-                    reducedVal.count += countObjVals[idx].count;
-                    reducedVal.qty += countObjVals[idx].qty;
+            console.log("Load specific firm page for " + firmName);
+            Firm.find({"Firm": firmName}, function(err, docs) {
+		if (err) return console.error(err);
+		console.log(docs);
+		if(req.session.user!=null){
+		    res.render('specificFirms', {
+			"forecast" : docs,
+			title: firmName,
+			loggedin: 'true',
+			uname: req.session.user.name
+		    });    
+		} else {
+		    res.render('specificFirms', {
+			"forecast" : docs,
+			title: firmName
+		    });
 		}
-		return reducedVal;
-            };
-            o.finalize = function (key, reducedVal) {
-		reducedVal.avg = reducedVal.qty/reducedVal.count;
-		return reducedVal;
-            };
-            o.out = 'map_reduce_results';
-	    o.verbose = true;
-	    o.query = {'Firm': firmName}
-            Firm.mapReduce(o, function (err, model, stats) {
-		if(err){
-                    console.log("Error: ", err);
-		}else{
-		    console.log("Load specific firm page for " + firmName);
-                    console.log("Success, Took %d ms", stats.processtime);
-		}
-		
-		model.find().exec(function(err, docs) {
-		    if (err) return console.error(err);
-		    console.log(docs);
-		    if(req.session.user!=null){
-			res.render('specificFirms', {
-			    "forecast" : docs,
-			    title: firmName,
-			    loggedin: 'true',
-			    uname: req.session.user.name
-			});    
-		    } else {
-			res.render('specificFirms', {
-			    "forecast" : docs,
-			    title: firmName
-			});
-		    }
-		});
-            });
-	} else {
+	    });
+        } else {
 	    res.redirect('/premium');
 	}
     };
